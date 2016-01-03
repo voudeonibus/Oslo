9335eb8ddd7a7c4945cdc21af1ba1337d7db95ec
Lucas <AgtLucas@users.noreply.github.com>
2016-01-03 14:23:42 -0200

    Standard JS

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-'use strict';
+'use strict'
 
 // Require modules
-var _ = require('lodash');
+var _ = require('lodash')
 
 module.exports = function(query, options) {
 
-  var queryDatabase = {};
+  var queryDatabase = {}
 
   _.mapKeys(options, function(value, key) {
 
     if (_.isArray(value)) {
-      var keyFind = false;
+      var keyFind = false
 
       _.each(value, function(item){
         if (_.has(query, item) && !keyFind && !_.isUndefined(query[item])) {
-          queryDatabase[key] = query[item];
-          keyFind = true;
+          queryDatabase[key] = query[item]
+          keyFind = true
         }
-      });
+      })
 
     } else if (_.isString(value) && !_.isUndefined(query[value])) {
 
-      queryDatabase[key] = query[value];
+      queryDatabase[key] = query[value]
 
     }
 
-  });
+  })
 
-  return queryDatabase;
+  return queryDatabase
 
-};
+}
